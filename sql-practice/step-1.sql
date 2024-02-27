46cae02dc9a773bb44375ccbec7393f7e2104a70
TheSaviourEking <saviour.eking@outlook.com>
2024-02-27 05:39:36 +0100

    chore: implement step-1.sql requirements

--- sql-practice/step-1.sql
+++ sql-practice/step-1.sql
@@ -1,3 +1,12 @@
 -- Step 1
 -- JOIN the tables, matching FOREIGN KEYs to the corresponding PRIMARY KEY.
 -- Your code here
+-- Create table
+-- .read seed-date.sql
+-- Step 1: SELECT the result of a joined table
+SELECT
+  bands.name,
+  albums.title
+FROM
+  bands
+  JOIN albums ON (bands.id = albums.band_id);
